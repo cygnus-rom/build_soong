4b39c21d43074c9091874f06f8f1d2fc56dccbdf
Simon Shields <simon@lineageos.org>
2017-08-23 12:05:33 +1000

    soong: extend with rockstar board flags
    Includes the following commit:
    
    Author: Simon Shields <simon@lineageos.org>
    Date:   Mon Sep 4 21:09:44 2017 +1000
    
        soong: explicitly specify name of wave variables struct
    
        Change-Id: Ic754b8a13658ddc40620f985581bd1754b780b15
    
    Change-Id: If127c9da6b57340ae57e0cf725151cdcc3c82ef3
    Signed-off-by: Ojas Sinha <sinhaojas67@gmail.com>

--- scripts/microfactory.bash
+++ scripts/microfactory.bash
@@ -59,7 +59,7 @@ function soong_build_go
     BUILDDIR=$(getoutdir) \
       SRCDIR=${TOP} \
       BLUEPRINTDIR=${TOP}/build/blueprint \
-      EXTRA_ARGS="-pkg-path android/soong=${TOP}/build/soong" \
+      EXTRA_ARGS="-pkg-path android/soong=${TOP}/build/soong -pkg-path rockstar/soong=${TOP}/vendor/rockstar/build/soong" \
       build_go $@
 }
 
